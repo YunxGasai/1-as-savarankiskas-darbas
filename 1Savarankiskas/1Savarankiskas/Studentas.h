158058b5d0bedfd629f930fd4fa1036230d63303
Deividas Jankauskas <104933871+YunxGasai@users.noreply.github.com>
2024-10-01 20:55:45 +0300

    Add files via upload

---
+++ 1Savarankiskas/1Savarankiskas/Studentas.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "myLib.h"
+
+class studentas {
+private:
+	string vard, pav;
+	vector <double> paz;
+	double egz, rez;
+public:
+	studentas();
+	studentas(string, string, vector<double>, double);
+	~studentas();
+
+	void printas();
+  
+	double vidurkis() const;
+	double mediana();
+
+	string vard_();
+	string pav_();
+	void s_vard(string);
+	void s_pav(string);
+};
