7562db0f9a1cd4c93d99f0edea0d5f1637b48d08
Deividas Jankauskas <104933871+YunxGasai@users.noreply.github.com>
2024-11-12 18:22:22 +0200

    Add files via upload

---
+++ 1-Savarankiskas V1.0/Source/mylib.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+
+using std::string;
+using std::vector;
+using std::copy;
+
+using std::to_string;
+using std::ifstream;
+using std::istringstream;
+using std::ofstream;
+
+
+
